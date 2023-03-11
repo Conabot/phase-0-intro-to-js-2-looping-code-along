89504f9df6fb118c7c35b9075e896056134b7032
Minh Tran <tdaimike@gmail.com>
2023-03-11 11:32:18 -0800

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solutions in this file
+
+function  writeCards(names, events) {
+    const messages = [];
+    for (let i = 0; i < names.length; i++) {
+        messages.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
+    }
+    return messages;
+}
+console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
+
+
+function countDown(number) {
+    while (number > 0) {
+        console.log(number);
+        number -= 1;
+    }
+    console.log(number);
+}
