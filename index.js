ae5c7bcd17ab8448a6adf6600331dfff08a797e9
Minh Tran <tdaimike@gmail.com>
2023-03-31 22:51:09 -0700

    complete FS

--- index.js
+++ index.js
@@ -1,19 +1,16 @@
 // Code your solutions in this file
-
-function  writeCards(names, events) {
+function writeCards(names) {
     const messages = [];
     for (let i = 0; i < names.length; i++) {
-        messages.push(`Thank you, ${names[i]}, for the wonderful ${events} gift!`);
-    }
+        messages.push (`Thank you, ${names[i]}, for the wonderful surprise gift!`);
+    };
     return messages;
 }
-console.log(writeCards(["Guadalupe", "Ollie", "Aki"], "surprise"));
-
 
 function countDown(number) {
     while (number > 0) {
         console.log(number);
         number -= 1;
     }
-    console.log(number);
+    console.log( number);
 }
